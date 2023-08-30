8e81a27c8e8f4566ad43a5869d564915453a33fe
Yendi Lestrade <65693440+Prezea@users.noreply.github.com>
2023-08-29 22:41:21 -0600

    1.- Exploración de datos

---
+++ exploracion de datos.R
@@ -0,0 +1,26 @@
+
+
+#___________ Práctica 1_______________
+# Exploración de datos
+
+
+# Se implementa la matriz iris que se encuentra cargada en R
+
+#--------------------------------------------------------
+#        Exploración de la Matriz  Iris
+#--------------------------------------------------------
+
+# 1.- Importación de la matriz
+data(iris)
+
+# 2.- Dimensión de la matriz
+dim(iris)
+
+# 3.- Nombre de las columnas
+colnames(iris)
+
+# 4.- Tipo de variables
+str(iris)
+
+# 5.- En busca de datos perdidos
+anyNA(iris)
